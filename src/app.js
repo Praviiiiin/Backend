e69babcb7195b331d1faa91419cc79dd87835f55
Pravin <epravin2005@gmail.com>
2026-04-26 00:50:00 +0530

    endpoints and registering user

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js"
+
+
+// const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server working");
+});
+
+
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser()); 
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }));
+
+
+
+app.use("/api/v1/users", userRouter);
+
+app.listen(8000, () => {
+  console.log("Server running on port 8000");
+});
+
+
+
